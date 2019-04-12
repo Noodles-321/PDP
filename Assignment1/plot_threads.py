7fd1c8faa50f9e73d819308fda0231c106807660
JiahaoLU <Jiahao.Lu.2199@student.uu.se>
2019-04-12 13:45:49 +0200

    update plotting and report

---
+++ Assignment1/plot_threads.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+#%%
+file1 = 'A1output'
+#time_serial = 6.823
+#cal_per = 0.885
+
+speedup_theo = list(range(1,33))
+
+with open(file1, 'r') as f:
+    ptime = [float(line.split()[2]) for line in f.readlines()[:32]]
+
+''' For average testing
+with open(file1, 'r') as f:
+    ptime1 = [float(line.split()[2]) for line in f.readlines()[:32]]
+with open(file1, 'r') as f:
+    ptime2 = [float(line.split()[2]) for line in f.readlines()[32:64]]
+with open(file1, 'r') as f:
+    ptime3 = [float(line.split()[2]) for line in f.readlines()[64:]]
+
+ptime = (np.array(ptime1) + np.array(ptime2) + np.array(ptime3)) / 3
+'''
+
+speedup = [ptime[0] / time for time in ptime]
+
+
+#%%
+plt.figure(figsize=(10, 6))
+plt.plot(list(range(1,33)), speedup_theo, 'g--', label='Theoretical')
+plt.plot(list(range(1,33)), speedup, '.-', label='Measured')
+
+plt.xlabel('Number of processors', fontsize=12)
+plt.ylabel('Speedup', fontsize=12)
+plt.title('Speedup Performance', fontsize=16)
+plt.legend()  # auto-legend with labels above.
+plt.savefig('speedup.png')
+plt.show()
+
+#%%
+'''
+with open(file1, 'r') as f:
+    reals = [line.split()[1] for 
+             line in f.readlines() if 
+             line.split() and line.split()[0] == 'real']
+with open(file1, 'r') as f:
+    users = [line.split()[1] for 
+             line in f.readlines() if 
+             line.split() and line.split()[0] == 'user']
+
+reals = [float(time.split('m')[0]) * 60 + 
+         float(time.split('m')[1][:-1]) for 
+         time in reals]
+users = [float(time.split('m')[0]) * 60 + 
+         float(time.split('m')[1][:-1]) for 
+         time in users]
+'''
--- README.md
+++ README.md
@@ -9,5 +9,20 @@ FuckWorld
 
   > Study the parallel performance and the scalability of your implementation, considering both fixed problem size and scaled problem size scalability.
 
-- [ ] Plot speedup and **the shit above**
-- [ ] Report
+- [ ] Plot  **the shit above**
+
+- Report
+
+  - [ ] Solution: 
+
+    Task:
+
+    >To this end, divide the processes into a p*1 mesh as equally as possible, and assign a strip of the computational domain to each of them as illustrated in Figure 1.
+
+    What is the difference between above and **Trivial Implementation** bellow?
+
+    >Given p processes, we slice the sum into p pieces, attach one interval to each of them to compute a partial sum, and then collect the local sums into one process.
+
+  - [ ] Scalability testing method
+
+  - [ ] Discussion
